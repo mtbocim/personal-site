543d01d29ffe47dd2bdcf900394b20af43eec25b
Michael Bocim <mtbocim@gmail.com>
2023-04-22 17:59:34 -0600

    getting data and cookie

--- src/api.js
+++ src/api.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
+// axios.defaults.withCredentials = true;
 
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
 
 class siteApi {
     static async counter() {
-        let res = await axios.post(`${BASE_URL}/counter`)
+        let res = await axios({
+            method:"post",
+            url:`${BASE_URL}/counter`,
+            withCredentials:true
+        })
         return res;
     }
 }
