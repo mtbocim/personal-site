68ee73fdd7c286a78e046915f71dd412897eaa3f
Michael Bocim <mtbocim@gmail.com>
2023-04-26 22:15:21 -0600

    styling for phone size

--- src/VistorCounter.js
+++ src/VistorCounter.js
@@ -2,14 +2,16 @@ import './VisitorCounter.css'
 
 import CounterDisplay from './CounterDisplay'
 
-function VisitorCounter({count, fact}){
+function VisitorCounter({ count, fact }) {
     let strCount = count.toString()
-    let completeStrCount = '0'.repeat(8-strCount.length)+strCount   
-    return(
+    let completeStrCount = '0'.repeat(8 - strCount.length) + strCount
+    return (
         <div className="VisitorCounter logical">
-            <CounterDisplay strValue={completeStrCount}/>
-            <p className='VisitorCounter-fact creative'> {fact}</p>
-            <p>(Visitor fact brought to you by NumbersAPI!)</p>
+            <CounterDisplay strValue={completeStrCount} />
+            <div className='VisitorCounter-fact'>
+                <p className='creative'> {fact}</p>
+                <p>(Visitor fact brought to you by NumbersAPI!)</p>
+            </div>
         </div>
     )
 }
--- src/projects/ProjectDisplay.jsx
+++ src/projects/ProjectDisplay.jsx
@@ -1,14 +1,21 @@
 import './ProjectDisplay.css'
 
-function ProjectDisplay(){
-    return(
+function ProjectDisplay() {
+    return (
         <div className="ProjectDisplay">
             <h3>Projects are hosted on free servers, please allow some time for them to spin up</h3>
-            <img className="ProjectDisplay-project" src={require("../assets/Jobly-Screenshot.png")}></img>
-            <a href='https://chic-stardust-c1d1e8.netlify.app/'>Jobly Demo</a>
-            <img className="ProjectDisplay-project" src={require("../assets/Warbler-Screenshot.png")}></img>
-            <a href='https://michael-bocim-warbler.onrender.com'>Warbler demo</a>
-            
+            <div className='ProjectDisplay-project'>
+                <a href='https://chic-stardust-c1d1e8.netlify.app/'>
+                    Jobly Demo
+                    <img className="ProjectDisplay-project" src={require("../assets/Jobly-Screenshot.png")}></img>
+                </a>
+            </div>
+            <div className='ProjectDisplay-project'>
+                <a href='https://michael-bocim-warbler.onrender.com'>
+                    Warbler demo
+                    <img className="ProjectDisplay-project" src={require("../assets/Warbler-Screenshot.png")}></img>
+                </a>
+            </div>
         </div>
     )
 }
