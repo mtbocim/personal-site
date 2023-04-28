9c1cee9e3217983b84237aacb9485c73efa609e0
michael-bocim <mtbocim@gmail.com>
2023-04-27 23:32:28 -0600

    phone styling

--- src/projects/ProjectDisplay.jsx
+++ src/projects/ProjectDisplay.jsx
@@ -5,13 +5,20 @@ function ProjectDisplay() {
         <div className="ProjectDisplay">
             <h3>Projects are hosted on free servers, please allow some time for them to spin up</h3>
             <div className='ProjectDisplay-project'>
-                <a href='https://chic-stardust-c1d1e8.netlify.app/'>
+                <a 
+                    href='https://chic-stardust-c1d1e8.netlify.app/'
+                    className='logical'
+                >
                     Jobly Demo
                     <img className="ProjectDisplay-project" src={require("../assets/Jobly-Screenshot.png")}></img>
                 </a>
             </div>
             <div className='ProjectDisplay-project'>
-                <a href='https://michael-bocim-warbler.onrender.com'>
+                <a 
+                    href='https://michael-bocim-warbler.onrender.com'
+                    className='logical'
+                >
+
                     Warbler demo
                     <img className="ProjectDisplay-project" src={require("../assets/Warbler-Screenshot.png")}></img>
                 </a>
